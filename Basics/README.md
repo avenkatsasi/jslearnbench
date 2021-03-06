22dc8c278a0eedea17710d38f6a87b1b33ecd8e9
sasilevlivet <sasi@levlivet.net>
2021-03-06 20:03:28 +0530

    first commit

---
+++ Basics/README.md
@@ -0,0 +1,8 @@
+Learn and try concepts that cover all basic topics of JS
+- variables
+- loops
+- functions
+- classes
+- objects
+- memory concepts
+- Interaction with web-pages
---
+++ Basics/index.js
@@ -0,0 +1,8 @@
+let topic = 'JavaScript Basics'
+console.log('Welcome to ' + topic)
+
+function consoleGreet(name) {
+    console.log('welcome ' + name)
+}
+
+consoleGreet('Venkat')
